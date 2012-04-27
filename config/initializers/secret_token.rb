d9fddcd78c484d7a769fbff55813fd3ab687bffa
Jon Snyder <jon@contextoptional.com>
2012-04-27 12:05:50 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monopoly::Application.config.secret_token = 'b065eea2cec2b4f35722d820fb1634f1122a16a0c60db1afceb67e0f7c20aa66cea31d58639f63cc593331e460c7ad95cc394abae8ba2540e1e836779a4d0a11'
